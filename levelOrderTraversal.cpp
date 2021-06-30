6c86d299b4e50cb369febc97404ef7ad98a4f42a
Nikhil Shinde <65338421+niks333@users.noreply.github.com>
2021-06-30 17:32:16 +0530

    Update levelOrderTraversal.cpp

--- levelOrderTraversal.cpp
+++ levelOrderTraversal.cpp
@@ -1,3 +1,6 @@
+	/*THIS IS MY FIRST TIME CODE WITH THE HELP OF ALGO*/
+
+/*
 class Solution
 {
     public:
@@ -48,5 +51,58 @@ class Solution
         printLevelOrder( node, &output );
      
         return output;
+    }
+};
+*/
+	/*THIS IS MY CODE*/
+
+//this function takes root node as input and returns height of that tree
+int height(Node *node){
+    //this is a recursive function
+    if(node==NULL) return 0;//this is the base case
+    //main code
+    //creat two variables for storing heights of left and right subtree of current node
+    int lheight=height(node->left);
+    int rheight=height(node->right);
+    
+    //who have max height will be height of tree
+    if(lheight>rheight){
+        return (lheight+1);
+    }
+    else{
+        return (rheight+1);
+    }
+}
+
+//this function adds elements in output array
+void addElements(int d,Node* node,vector<int> *output){
+    //this is a recursive function
+    if(node==NULL) return;
+    //if height is 1 then we have to print value at that node
+    if(d==1){
+        output->push_back(node->data);
+    }
+    //if height is greater than 1 then we must calculate for each subtree
+    addElements(d-1,node->left,output);
+    addElements(d-1,node->right,output);
+}
+
+class Solution
+{
+    public:
+    //Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node* node)
+    {
+      //as we have to return a vector as a output first create one
+      vector<int> output;
+      
+      //now we have to calculate height of tree for that we will wirte a function height()
+      int h=height(node);
+      //now we will add level wise elements in our output array
+      for(int i=1;i<=h;i++){
+          addElements(i,node,&output);
+      }
+      //now we have our values in output array
+      return output;
     }
 };
